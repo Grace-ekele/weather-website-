453a815139cc5ed88126abbd66b6e48292a5e2f0
Grace Ekele <ekelegrace.ge@gmail.com>
2024-09-16 05:17:40 +0100

    made some changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
@@ -101,6 +102,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(port, () => {
+  console.log("server is running on port " + port);
 });
